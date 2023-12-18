871da86c22885b8c594103ca7ce75e31a8c87d52
唯人 佐藤 <220079@jc-21.jp>
2023-12-18 10:49:26 +0900

    とりあえずできるところまで

--- MyGameEngine/Simple3D.hlsl
+++ MyGameEngine/Simple3D.hlsl
@@ -80,34 +80,25 @@ float4 PS(VS_OUT inData) : SV_Target
 
 	
 	//ŠgŽU”½ŽË‚Ì’l‚ð•Ô‚·(?)
-	float4 nk;
-	if (inData.color.x < 1 / 3.0)
-	{
-		nk = float4(0.0,0.0,0.0,0.0);
-	}
-	else if (inData.color.x < 2 / 3.0)
-	{
-		nk = float4(0.5,0.5,0.5,0.5);
-	}
-	else
-	{
-		nk = float4(1.0,1.0,1.0,1.0);
-	}
+	float4 n1 = float4(1 / 4.0, 1 / 4.0, 1 / 4.0, 1);
+	float4 n2 = float4(2 / 4.0, 2 / 4.0, 2 / 4.0, 1);
+	float4 n3 = float4(3 / 4.0, 3 / 4.0, 3 / 4.0, 1);
+	float4 n4 = float4(4 / 4.0, 4 / 4.0, 4 / 4.0, 1);
+	float4 tI = 0.1 * step(n1, inData.color) + 0.3 * step(n2, inData.color) 
+		        + 0.3*step(n3, inData.color)+0.4*step(n4,inData.color);
 
-
-	
 	if (isTexture == false)
 	{
-		diffuse = lightSource * diffuseColor * inData.color;
+		diffuse = lightSource * diffuseColor * tI;
 		ambient = lightSource * diffuseColor * ambentSource;
 	}
 	else
 	{
-		diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;
+		diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * tI;
 		ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambentSource;
 	}
 
-	return nk;
+	return diffuse+ambient;
 
 	//return float4 (1,1,1,1);
 
