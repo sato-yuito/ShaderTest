5d8680cbf926485cfb905c184c30018b800a1ca4
唯人 佐藤 <220079@jc-21.jp>
2024-01-30 16:33:49 +0900

    とりあえずできたと思うからオブジェクト二つだしたらできると思うー

---
+++ MyGameEngine/Dice.cpp
@@ -0,0 +1,25 @@
+#include "Dice.h"
+
+Dice::Dice(GameObject* parent)
+{
+}
+
+Dice::~Dice()
+{
+}
+
+void Dice::Initialize()
+{
+}
+
+void Dice::Update()
+{
+}
+
+void Dice::Draw()
+{
+}
+
+void Dice::Release()
+{
+}
